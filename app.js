36f0b5a2fb7407f7ba51257fc12b71632422d82b
manucarenjot <carenjotje@gmail.com>
2021-11-29 15:33:29 +0100

    Date

---
+++ app.js
@@ -0,0 +1,20 @@
+let result = document.getElementById('result');
+
+let aujourdhui = new Date();
+
+result.innerHTML =
+    "jour de la semaine" + aujourdhui.getDay() + "<br/>" +
+    "Jour du mois:" + aujourdhui.getDate() + '<br/>' +
+    'Mois en cours version JS:' + (aujourdhui.getMonth() + 1) + '<br/>'+
+    "Année au format 4 chiffres:" + aujourdhui.getFullYear() + '<br/>'+
+    "Année au format 2 chiffres:" + aujourdhui.getFullYear().toString().slice(2) + '<br/>'+
+    "Heure:"+ aujourdhui.getHours()+ "<br/>"+
+    "Heure avec un zero devant si besoin"+ aujourdhui.getHours().toString().padStart(2, 0) + "<br/>"+
+    "Minutes:" + aujourdhui.getMinutes() + "<br/>" +
+    "Minutes avec un zero devant si besoin:" + aujourdhui.getMinutes().toString().padStart(2, 0) + "<br/>"+
+    "seconde:" + aujourdhui.getSeconds() + "<br/>" +
+    "Milliseconde:" + aujourdhui.getMilliseconds()
+;
+
+
+
