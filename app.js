b637ebe9b76e70e40cd42c7902ae0f09dfa9ab26
manucarenjot <carenjotje@gmail.com>
2021-11-29 15:48:23 +0100

    Date

--- app.js
+++ app.js
@@ -13,8 +13,15 @@ result.innerHTML =
     "Minutes:" + aujourdhui.getMinutes() + "<br/>" +
     "Minutes avec un zero devant si besoin:" + aujourdhui.getMinutes().toString().padStart(2, 0) + "<br/>"+
     "seconde:" + aujourdhui.getSeconds() + "<br/>" +
-    "Milliseconde:" + aujourdhui.getMilliseconds()
+    "Milliseconde:" + aujourdhui.getMilliseconds() + "<br/>" + "<br>"
 ;
 
 
+aujourdhui = new Date();
+aujourdhui.setHours(20);
+aujourdhui.setMinutes(68);
 
+result.innerHTML +=
+    "Heure:"  + aujourdhui.getHours() + "<br>" +
+    "Minutes:" + aujourdhui.getMinutes()
+;
